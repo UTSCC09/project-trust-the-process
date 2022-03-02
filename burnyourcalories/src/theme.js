52f060f739bb68f07973ea603a5404713404c32c
Prashanth Keth <55007300+prashketh@users.noreply.github.com>
2022-03-01 21:00:23 -0500

    Added font

--- burnyourcalories/src/theme.js
+++ burnyourcalories/src/theme.js
@@ -1,6 +1,6 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createTheme } from '@material-ui/core/styles'
 
-const defaultTheme = createMuiTheme({
+const defaultTheme = createTheme ({
 	palette: {
 		background: {
 			default: '#ececec', // #282c34
