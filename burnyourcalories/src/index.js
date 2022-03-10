f8a125c6ae910f8bba5fb70fd6ff9daf9fc32100
Prashanth Ketheeswaran <prashanthketheeswaran@gmail.com>
2022-03-10 18:04:08 -0500

    Committing changes

--- burnyourcalories/src/index.js
+++ burnyourcalories/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient, 
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,17 +16,26 @@ import theme from './theme';
 import Shoppies from './pages/index';
 import Welcome from './pages/welcome';
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:3001',
+  })
+})
+
 ReactDOM.render(
   <Router>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routes>
-        {/* Example path */}
-        <Route exact path="/example" element={<Shoppies />} />
-        {/* Sign-in/up page */}
-        <Route exact path="/welcome" element={<Welcome view={'signin'}/>} />
-      </Routes>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          {/* Example path */}
+          <Route exact path="/example" element={<Shoppies />} />
+          {/* Sign-in/up page */}
+          <Route exact path="/welcome" element={<Welcome view={'signin'}/>} />
+        </Routes>
+      </ThemeProvider>
+    </ApolloProvider>
   </Router>
   ,
   document.getElementById('root')
