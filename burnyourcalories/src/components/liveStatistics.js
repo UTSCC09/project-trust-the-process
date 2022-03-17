81d0c3cdb687e9c61e22905acbab77fa19342939
Vignesh <vigneshnanthak@gmail.com>
2022-03-17 18:02:43 -0400

    updated live statistics style with material ui

--- burnyourcalories/src/components/liveStatistics.js
+++ burnyourcalories/src/components/liveStatistics.js
@@ -1,76 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { LockOutlined } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles';
-import { createTheme } from '@mui/material/styles';
-
-import {
-	Container,
-	Box,
-	Typography,
-	Avatar,
-	TextField,
-} from '@mui/material';
-
-import {
-    useMutation, 
-    gql 
-} from '@apollo/client';
-
-import { BrowserRouter as Router,
-    Navigate,
-    Route,
-    Routes,
-    useNavigate
-} from 'react-router-dom';
-
-
-
-
-
-
-
-
-
-
 import { ExerciseStat } from "./exerciseStat";
+import { Typography } from '@mui/material';
 
-
-export default function LiveStatistic(props) {
-    const { exercises, totalWorkoutTime } = props;
-
-    return (
-      <div className="LiveStat">
-
-        <div className="Exercises">
-          {exercises.length === 0 ? (
-            <h2>No exercises added yet, try adding some.</h2>
-          ) : (
-            exercises.map((exercise, idx) => (
-              <ExerciseStat
-                exercise={exercise}
-              />
-            ))
-          )}
-        </div>
-
-
-        <div className="WorkoutDuration">
-          {totalWorkoutTime}
-        </div>
-
-      </div>
-    );
-}
-
-
-const divStyle = {
-  color: 'blue',
-  align: 'center',
-};
+const useStyles = makeStyles(() => ({
+  liveStat: {
+    width: 300,
+    height: 500,
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+  },
+}));
 
 LiveStatistic.defaultProps = {
   exercise: [],
   totalWorkoutTime: 100
+};
+
+export default function LiveStatistic(props) {
+    const { exercises, totalWorkoutTime } = props;
+    const classes = useStyles();
+
+    return (
+      <div className={classes.liveStat}>
+        <Typography align="centre" variant="h2">
+          Live Statistics
+        </Typography>
+
+        {exercises.length === 0 ? (
+          <h2>No exercises added yet, try adding some.</h2>
+        ) : (
+          exercises.map((exercise, idx) => (
+            <ExerciseStat
+              exercise={exercise}
+            />
+          ))
+        )}
+        
+        <Typography align="centre" variant="h6">
+          {totalWorkoutTime}
+        </Typography>
+      </div>
+    );
 }
-
-
