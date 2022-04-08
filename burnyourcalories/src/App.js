9a42656a0c75affd6f90b2f763f867bc7200e373
Vignesh <vigneshnanthak@gmail.com>
2022-04-08 18:27:33 -0400

    added deployment to main

--- burnyourcalories/src/App.js
+++ burnyourcalories/src/App.js
@@ -17,7 +17,7 @@ import Dashboard from './pages/dashboard';
 import Reports from './pages/reports';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 // REFERENCE: https://www.apollographql.com/docs/react/networking/authentication/
